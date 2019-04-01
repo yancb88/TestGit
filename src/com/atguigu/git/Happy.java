d8c3bd3a114c87259b538c047a514cbd5416fd7b
eclipse_user <eclipse_user@qq.com>
2019-04-01 21:06:43 +0800

    Happy文件提交

---
+++ src/com/atguigu/git/Happy.java
@@ -0,0 +1,27 @@
+package com.atguigu.git;
+
+public class Happy {
+	
+	private int i;
+	
+	private String name;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
