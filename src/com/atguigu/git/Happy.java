7f30e51868ae6993c4762f7294e5a9e046490ec8
eclipse_user <eclipse_user@qq.com>
2019-04-11 14:08:10 +0800

    综合分析功能代码提交

--- src/com/atguigu/git/Happy.java
+++ src/com/atguigu/git/Happy.java
@@ -2,24 +2,8 @@ package com.atguigu.git;
 
 public class Happy {
 	
-	private int i;
-	
-	private String name;
-
-	public int getI() {
-		return i;
-	}
-
-	public void setI(int i) {
-		this.i = i;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
+	public static void main(String[] args) {
+		System.out.println("综合分析改造");
 	}
 	
 	
